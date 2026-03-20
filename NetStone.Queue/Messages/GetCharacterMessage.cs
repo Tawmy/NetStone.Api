a629cf0518e02f78f63cc01d877cea863473a200
Tawmy <git@tawmy.dev>
2026-03-20 12:50:52 +0100

    Remove NetStone.Queue project

--- NetStone.Queue/Messages/GetCharacterMessage.cs
+++
@@ -1,5 +0,0 @@
-using NetStone.Common.Enums;
-
-namespace NetStone.Queue.Messages;
-
-public record GetCharacterMessage(string LodestoneId, int? MaxAge, bool? CacheImages, FallbackTypeV4? UseFallback);
